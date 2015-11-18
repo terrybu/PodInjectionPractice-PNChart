b91f8d6f25b74536cb4a11bf4cb24639ff904b0c
Terry Bu <burea1124@gmail.com>
2015-11-18 15:22:42 -0500

    first commit

--- PNPieChartInjectionPractice/ViewController.swift
+++ PNPieChartInjectionPractice/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import PNChart
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let item1 = PNPieChartDataItem(value: 20, color: UIColor.blueColor(), description: "Stat1")
+        let item2 = PNPieChartDataItem(value: 50, color: UIColor.greenColor(), description: "Stat2")
+        let item3 = PNPieChartDataItem(value: 30, color: UIColor.brownColor(), description: "Stat3")
+        let items = [item1, item2, item3]
+        let pieChart = PNPieChart(frame: CGRect(x: 10, y: 10, width: 300, height: 300), items: items)
+        pieChart.strokeChart()
+        view.addSubview(pieChart)
+
+        
     }
 
     override func didReceiveMemoryWarning() {
